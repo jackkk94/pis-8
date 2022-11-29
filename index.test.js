6ceda3ee6630fe35b55eca75898f155af31f48dd
jack panchenko <jackpanchenko@MacBook-Pro-jack.local>
2022-11-30 02:20:33 +0300

    tests

--- index.test.js
+++ index.test.js
@@ -1,10 +1,7 @@
-import request from 'supertest';
 import { it, describe } from 'mocha';
-// eslint-disable-next-line import/extensions
+// eslint-disable-next-line import/extensions, no-unused-vars
 import app from './index.js';
 
 describe('Test Counter routes', () => {
-  it('Test /about', () => (()=> '<h3>Hello, jack!</h3>').expect('<h3>Hello, jack!</h3>'));
-
-
+  it('Test /about', () => (() => '<h3>Hello, jack!</h3>').expect('<h3>Hello, jack!</h3>'));
 });
