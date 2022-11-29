3dfe831cb4829498c0ee7e4f29f9c04ab2d8030a
jack panchenko <jackpanchenko@MacBook-Pro-jack.local>
2022-11-30 01:04:12 +0300

    actions

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import Express from 'express';
-const app = Express()
+
+const app = Express();
 let counter = 0;
 
-console.log("hello")
-app.get('/',(_, response) => {
+// eslint-disable-next-line no-console
+console.log('hello');
+app.get('/', (_, response) => {
   response.send(`${counter}`);
 });
 
-app.get('/stat',(_, response) => {
+app.get('/stat', (_, response) => {
+  // eslint-disable-next-line no-plusplus
   response.send(`${++counter}`);
 });
 
-app.get('/about',(_, response) => {
-  response.send(`<h3>Hello, jack!</h3>`);
+app.get('/about', (_, response) => {
+  response.send('<h3>Hello, jack!</h3>');
 });
 
-
-//app.listen(3000);
+// app.listen(3000);
